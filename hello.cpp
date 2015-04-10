64179546678da75642995208f0cbb4ac80626f22
Sorina Maciuca <sm0179@well.ox.ac.uk>
2015-04-10 09:58:40 +0100

    Hello World test

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include<ostream>
+#include<sstream>
+
+void helloworld(std::ostream &os){
+  os<<"Hello World";
+}
---
+++ hello_unittest.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "hello.h"
+#include <sstream>
+
+TEST(TestHello, hello) 
+{
+  std::ostringstream output;
+  helloworld( output );
+  EXPECT_STREQ("Hello World",output.str().c_str());
+    }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
