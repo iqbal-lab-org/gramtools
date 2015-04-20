0a314fe4c3f03d8dea37b580814acff860622c99
Zamin Iqbal <zam@well.ox.ac.uk>
2015-04-20 12:40:34 +0100

    Added some comments/questions

--- src/bidir_search.cpp
+++ src/bidir_search.cpp
@@ -3,11 +3,15 @@
 #include <cassert>
 #include "bwt_search.h"
 
+
+//what does wt stand for?
+//not super keen on a variable called l
+//could you maybe add a comment(s) explaining what the arguments are?
 uint32_t bidir_search(csa_wt<wt_int<rrr_vector<63>>> csa, uint32_t& l, uint32_t& r, uint32_t& l_rev, uint32_t& r_rev, uint32_t c)
 {
   assert(l < r); assert(r <= csa.size());
-  uint32_t c_begin = csa.C[csa.char2comp[c]];
-  auto r_s_b =  csa.wavelet_tree.lex_count(l, r, c);
+  uint32_t c_begin = csa.C[csa.char2comp[c]];//what does this do?
+  auto r_s_b =  csa.wavelet_tree.lex_count(l, r, c);//what is r_s_b? what do they stand for?
   uint32_t rank_l = std::get<0>(r_s_b);
   uint32_t s = std::get<1>(r_s_b) 
   uint32_t b = std::get<2>(r_s_b);
