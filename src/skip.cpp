25a5b268bfa567b744812465d80a14816d96eae2
Sorina Maciuca <sm0179@well.ox.ac.uk>
2015-08-17 16:56:24 +0100

    modified assertion because the search is for a num that has already been detected by range_search_2d, so the interval should always contain something

--- src/skip.cpp
+++ src/skip.cpp
@@ -83,7 +83,7 @@ bool skip(csa_wt<wt_int<bit_vector,rank_support_v5<>>> csa,
     right_rev=site_end+1;
   }
 
-  assert(right>=left);
+  assert(right>left);
   assert(right_rev-left_rev == right-left);
 
   return (last);
