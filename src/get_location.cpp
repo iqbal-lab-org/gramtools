30a39170ca658b3d876d1ba497e821002c85c39a
Sorina Maciuca <sm0179@well.ox.ac.uk>
2015-08-21 14:53:33 +0100

    removed left and right from args of get_location()

--- src/get_location.cpp
+++ src/get_location.cpp
@@ -8,7 +8,7 @@
 using namespace sdsl;
 
 uint32_t get_location(csa_wt<wt_int<bit_vector,rank_support_v5<>>> csa,
-		      uint64_t left, uint64_t right,
+		      uint64_t num_idx,
 		      uint32_t num, bool last, 
 		      std::vector<int>& allele, std::vector<int> mask_a)
 {
@@ -17,16 +17,9 @@ uint32_t get_location(csa_wt<wt_int<bit_vector,rank_support_v5<>>> csa,
         if (!last) allele.push_back(1);
       }
   else {
-    if (right-left==1) {
       site=num-1;
-      allele.push_back(mask_a[csa[left]]);
+      allele.push_back(mask_a[csa[num_idx]]);
     }
-    else {
-      site=num-1;
-      for (uint64_t i=left;i<right;i++)
-        allele.push_back(mask_a[csa[i]]);
-    }
-  }
   return (site);    
 }  
 
