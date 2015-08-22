14f2032ea3f9c2da8183573891f333f3977b4bb1
Sorina Maciuca <sm0179@well.ox.ac.uk>
2015-08-22 15:22:25 +0100

    save path of (site,allele) for each match

--- src/bidir_search_bwd.cpp
+++ src/bidir_search_bwd.cpp
@@ -13,65 +13,94 @@ std::vector<uint8_t>::iterator bidir_search_bwd(csa_wt<wt_int<bit_vector,rank_su
 		      std::vector<uint8_t>::iterator pat_begin, std::vector<uint8_t>::iterator pat_end,
 		      std::list<std::pair<uint64_t,uint64_t>>& sa_intervals, std::list<std::pair<uint64_t,uint64_t>>& sa_intervals_rev)
 {
+  std::list<std::vector<std::pair<uint32_t, std::vector<int>>>> sites;
+  std::list<std::vector<std::pair<uint32_t, std::vector<int>>>>::iterator it_s;
   std::vector<uint8_t>::iterator pat_it=pat_end;
   std::list<std::pair<uint64_t,uint64_t>>::iterator it, it_rev;
   uint8_t c=*pat_it;
-  bool last, first_del=false;
+  bool last, first_del=false,ignore;
   uint64_t left_new, right_new, left_rev_new, right_rev_new;
+  std::vector<std::pair<uint32_t, std::vector<int>>> empty_pair_vector;
+  std::vector<int> allele_empty;
 
   assert(left<right);
   assert(right<=csa.size());
 
   sa_intervals.push_back(std::make_pair(left,right));
   sa_intervals_rev.push_back(std::make_pair(left_rev,right_rev));
+  sites.push_back(empty_pair_vector);
 
   while (pat_it>pat_begin && !sa_intervals.empty()) {
     --pat_it;
     
     assert(sa_intervals.size()==sa_intervals_rev.size());
-    for(it=sa_intervals.begin(), it_rev=sa_intervals_rev.begin(); it!=sa_intervals.end() && it_rev!=sa_intervals_rev.end(); ++it, ++it_rev) {
+    assert(sa_intervals.size()==sites.size());
+
+    it=sa_intervals.begin();
+    it_rev=sa_intervals_rev.begin();
+    it_s=sites.begin();
+
+    for(;it!=sa_intervals.end() && it_rev!=sa_intervals_rev.end() && it_s!=sites.end(); ++it, ++it_rev, ++it_s) {
       auto res= csa.wavelet_tree.range_search_2d((*it).first, (*it).second-1, 5, maxx).second;
-      //res is going to contain sometimes multiple 5's for ex, non adjacent
+      //might want to sort res based on pair.second - from some examples it looks like sdsl already does that so res is already sorted 
+      uint32_t prev_num=0;
       for (auto z : res) {
-	auto i=z.first;
-	auto num=z.second;
+	uint64_t i=z.first;
+	uint32_t num=z.second;
+
+	if (num==prev_num && num%2==0) ignore=true;
+	else ignore=false;
 
 	left_new=(*it).first;
 	right_new=(*it).second;
+
+	//need original [l,r] to for the next loop iterations
 
 	left_rev_new=(*it_rev).first;
 	right_rev_new=(*it_rev).second;
 
 	last=skip(csa,left_new,right_new,left_rev_new,right_rev_new,num);
+	
+	
 	//call location here
-	//need original [l,r] to for the next loop iterations
 	// how to alternate between forward and backward?
-	if (it==sa_intervals.begin() && first_del==false) {
+	if (it==sa_intervals.begin() && first_del==false && !ignore) {
 	  sa_intervals.push_back(std::make_pair(left_new,right_new));
 	  sa_intervals_rev.push_back(std::make_pair(left_rev_new,right_rev_new));
+	  sites.push_back(std::vector<std::pair<uint32_t, std::vector<int>>>(get_location(csa,i,num,last,allele_empty,mask_a)));
 	}
 	else {
-	  *it=std::make_pair(left_new,right_new);
-	  *it_rev=std::make_pair(left_rev_new,right_rev_new);
+	  if (ignore) sites.back().back()=get_location(csa,i,num,last,sites.back().back().second,mask_a);
+	  else {
+	    *it=std::make_pair(left_new,right_new);
+	    *it_rev=std::make_pair(left_rev_new,right_rev_new);
+	    (*it_s).push_back(get_location(csa,i,num,last,allele_empty,mask_a));
+	  }
 	}
+	prev_num=num;  
       }
     }
 
     assert(sa_intervals.size()==sa_intervals_rev.size());
+    assert(sa_intervals.size()==sites.size());
+
     it=sa_intervals.begin();
-    it_rev=sa_intervals_rev.begin();			
+    it_rev=sa_intervals_rev.begin();	
+    it_s=sites.begin();
 		  
-    while (it!=sa_intervals.end() && it_rev!=sa_intervals_rev.end()) {	
+    while (it!=sa_intervals.end() && it_rev!=sa_intervals_rev.end() && it_s!=sites.end()) {	
       //calculate sum to return
       if (bidir_search(csa,(*it).first,(*it).second,(*it_rev).first,(*it_rev).second,c)>0) {
 	++it;
 	++it_rev;
+	++it_s;
       }
       else {
 	if (it==sa_intervals.begin()) first_del=true;
 	//might need to see first_del from top fcns to check if there are matches in the reference
 	it=sa_intervals.erase(it);
-	it_rev=sa_intervals_rev.erase(it);
+	it_rev=sa_intervals_rev.erase(it_rev);
+	it_s=sites.erase(it_s);
       }
     }
   }
--- src/get_location.cpp
+++ src/get_location.cpp
@@ -1,17 +1,20 @@
 #include "sdsl/suffix_arrays.hpp"
 #include "sdsl/wavelet_trees.hpp"
 #include <cassert>
-#include "bwt_search.h"
+//#include "bwt_search.h"
 #include <tuple>
 #include <cstdint>
 
 using namespace sdsl;
 
-uint32_t get_location(csa_wt<wt_int<bit_vector,rank_support_v5<>>> csa,
+std::pair<uint32_t, std::vector<int>> get_location(csa_wt<wt_int<bit_vector,rank_support_v5<>>> csa,
 		      uint64_t num_idx,
-		      uint32_t num, bool last, 
-		      std::vector<int>& allele, std::vector<int> mask_a)
+		      uint32_t num, bool last,
+		      std::vector<int>& allele,
+		      std::vector<int> mask_a)
 {
+  uint32_t site;
+
   if (num%2==1) {
  	site=num;
         if (!last) allele.push_back(1);
@@ -20,7 +23,7 @@ uint32_t get_location(csa_wt<wt_int<bit_vector,rank_support_v5<>>> csa,
       site=num-1;
       allele.push_back(mask_a[csa[num_idx]]);
     }
-  return (site);    
+  return (std::make_pair(site,allele));    
 }  
 
 //removed left and right args because they only form a proper interval is when last is true; but then they can't help with location because allele is unknown and we're just addin site
