fb1eca90a6866bd87414052baff3dbaf1d63404e
Sorina Maciuca <sm0179@well.ox.ac.uk>
2016-01-07 18:06:28 +0000

    fixed res declaration

--- src/bidir_search_bwd.cpp
+++ src/bidir_search_bwd.cpp
@@ -13,7 +13,8 @@ std::vector<uint8_t>::iterator bidir_search_bwd(csa_wt<wt_int<bit_vector,rank_su
 		      std::vector<uint8_t>::iterator pat_begin, std::vector<uint8_t>::iterator pat_end,
 		      std::list<std::pair<uint64_t,uint64_t>>& sa_intervals, 
 		      std::list<std::pair<uint64_t,uint64_t>>& sa_intervals_rev,
-		      std::list<std::vector<std::pair<uint32_t, std::vector<int>>>>& sites)
+		      std::list<std::vector<std::pair<uint32_t, std::vector<int>>>>& sites
+		      std::vector<int> mask_a, uint64_t maxx)
 {
   std::list<std::vector<std::pair<uint32_t, std::vector<int>>>>::iterator it_s;
   std::vector<uint8_t>::iterator pat_it=pat_end;
@@ -23,6 +24,7 @@ std::vector<uint8_t>::iterator bidir_search_bwd(csa_wt<wt_int<bit_vector,rank_su
   uint64_t left_new, right_new, left_rev_new, right_rev_new;
   std::vector<std::pair<uint32_t, std::vector<int>>> empty_pair_vector;
   std::vector<int> allele_empty;
+  std::vector<std::pair<uint64_t,uint64_t>> res;   
 
   assert(left<right);
   assert(right<=csa.size());
@@ -42,7 +44,7 @@ std::vector<uint8_t>::iterator bidir_search_bwd(csa_wt<wt_int<bit_vector,rank_su
     it_s=sites.begin();
 
     for(;it!=sa_intervals.end() && it_rev!=sa_intervals_rev.end() && it_s!=sites.end(); ++it, ++it_rev, ++it_s) {
-      auto res= csa.wavelet_tree.range_search_2d((*it).first, (*it).second-1, 5, maxx).second;
+      res= csa.wavelet_tree.range_search_2d((*it).first, (*it).second-1, 5, maxx).second;
       //might want to sort res based on pair.second - from some examples it looks like sdsl already does that so res is already sorted 
       uint32_t prev_num=0;
       for (auto z : res) {
