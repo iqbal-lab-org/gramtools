a744eb53e86a4cf0db115832f6283213b11822b0
Sorina Maciuca <sm0179@well.ox.ac.uk>
2016-02-10 16:28:05 +0000

    updated header file with new csa_constr bool arg and added bidir_search_fwd

--- include/bwt_search.h
+++ include/bwt_search.h
@@ -28,7 +28,7 @@ template < typename SEQUENCE >
 
 using sequence_set = std::unordered_set< SEQUENCE, seq_hash<SEQUENCE> > ;
 
-csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa_constr(std::string fname, std::vector<std::vector<int>>& covgs, char* int_al_fname, char* memory_log_fname, char* csa_file);
+csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa_constr(std::string fname, std::vector<std::vector<int>>& covgs, char* int_al_fname, char* memory_log_fname, char* csa_file, bool fwd);
 
 void precalc_kmer_matches (csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa, int k,   
 			   sequence_map<std::vector<uint8_t>, std::list<std::pair<uint64_t,uint64_t>>>& kmer_idx, 
@@ -56,6 +56,16 @@ bool skip(csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa,
 	              uint32_t num);
 
 std::vector<uint8_t>::iterator bidir_search_bwd(csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa,
+						uint64_t left, uint64_t right,
+						uint64_t left_rev, uint64_t right_rev,
+						std::vector<uint8_t>::iterator pat_begin, std::vector<uint8_t>::iterator pat_end,
+						std::list<std::pair<uint64_t,uint64_t>>& sa_intervals,
+						std::list<std::pair<uint64_t,uint64_t>>& sa_intervals_rev,
+						std::list<std::vector<std::pair<uint32_t, std::vector<int>>>>& sites,
+						std::vector<int> mask_a, uint64_t maxx, bool& first_del);
+
+
+std::vector<uint8_t>::iterator bidir_search_fwd(csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa,
 						uint64_t left, uint64_t right,
 						uint64_t left_rev, uint64_t right_rev,
 						std::vector<uint8_t>::iterator pat_begin, std::vector<uint8_t>::iterator pat_end,
