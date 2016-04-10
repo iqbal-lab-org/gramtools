c6ea25e9b2f9dc422c0349d44815cb84c82599b6
Zamin Iqbal <zam@well.ox.ac.uk>
2016-04-10 23:42:10 +0100

    Tiny bit of implementation, untested

--- utils/sitemarker.cpp
+++ utils/sitemarker.cpp
@@ -4,10 +4,53 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
-using namespace std;
+#include "sitemarker.hpp"
+
+//using namespace std;
 
 
-SiteMarker::SiteMarker(int id, int num_alleles)
+SiteMarker::SiteMarker(int id, int num_alleles):site_id(id)
 {
+  alleles = boost::dynamic_bitset<>( num_alleles ) ;
+}
+
+void SiteMarker::zero_all_alleles()
+{
+  alleles.reset();
+}
+void SiteMarker::set_allele(int i)
+{
+  alleles[i]=1;
+}
+
+void SiteMarker::set_these_alleles(std::vector<int> v)
+{
+  for (int i : v)
+    {
+      alleles[i]=1;
+    }
+}
+
+int SiteMarker::get_num_alleles()
+{
+  return alleles.size();
+}
+
+
+SiteMarkerArray::SiteMarkerArray(std::string filename_lin_prg)
+{
+  std::vector<int> num_alleles_in_site;
+  num_alleles_in_site.reserve(1000);
+  //open lin PRG file, parse it and count up number of sites and how many alleles each
+  //as you go through, for each site do:      num_alleles_in_site[i]=whatever
+
+  //then 
   
+}
+
+
+
+int main()
+{
+  return 1;
 }
--- utils/sitemarker.hpp
+++ utils/sitemarker.hpp
@@ -1,4 +1,4 @@
-
+#include <vector>
 #include <boost/dynamic_bitset.hpp>
 
 
@@ -11,10 +11,10 @@ public:
 
   SiteMarker(int id, int num_alleles);//constructor
   ~SiteMarker();
-  zero_all_alleles();
-  set_allele(int i);//set it to 1 if we cross this allele
-  set_these_alleles(std::vector<int>); //to set 5th allele, do    alleles[5]=1;
-  get_num_alleles(); // this returns alleles.size();
+  void zero_all_alleles();
+  void set_allele(int i);//set it to 1 if we cross this allele
+  void set_these_alleles(std::vector<int> v); //to set 5th allele, do    alleles[5]=1;
+  int get_num_alleles(); // this returns alleles.size();
 
 private:
   boost::dynamic_bitset<> alleles;  
@@ -26,29 +26,30 @@ private:
 //this object gets allocated once - parse the linPRG file
 // the sites vector has alphabet+1 entries. at index 0,1,2,3,4 - just have NULL pointers or something
 // then you index each site with the number it has in the linPRG. 
-class ArrayOfSiteMarkers
+class SiteMarkerArray
 {
 public:
-  ArrayOfSiteMarkers(std::string filename_lin_prg);//constructor
-
-
+  SiteMarkerArray(std::string filename_lin_prg);//constructor
+  
+  
   //suppose you want to get a pointer to the SiteMarker for site 56
   //and at the same time set alleles 1,5,6 to 1 (meaning this read overlaps alleles 1,5,6:
   SiteMarker* get_site_and_set_alleles(int site_id, std::vector<int> alleles);
-
+  
   
 private:
   std::vector<SiteMarker*> sites;
-}
-
+  int num_sites;
+};
+  
 
 //what is the thing we use when tracking which sites/alleles a read crosses?
 class SiteOverlapTracker
 {
 public:
-  SiteOverlapTracker(int num); //constructor - allocs the vector so it has 100 ptrs
-  vector<SiteMarker*> vec;
-}
+  SiteOverlapTracker(int num);
+  std::vector<SiteMarker*> vec;
+};
 //  vector<SiteMarker*> would do fine - an array of pointers, pointing to the fixed
 //  SiteMarkers sitting inside ArrayOfSiteMarkers.
 // PREALLOC this so it has 100 pointers.
