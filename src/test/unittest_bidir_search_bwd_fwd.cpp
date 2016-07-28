418389cff6b7ec23c4dc96a7135fe6e5b91a3b92
Zamin Iqbal <zam@well.ox.ac.uk>
2016-07-28 22:20:35 +0100

    second unit test now passing.

--- src/test/unittest_bidir_search_bwd_fwd.cpp
+++ src/test/unittest_bidir_search_bwd_fwd.cpp
@@ -71,24 +71,36 @@ TEST(BackwardSearchTest, NoVariants){
     p.clear();
   }
 }
+*/
 
 TEST(BackwardSearchTest, OneSNP){
 
-  csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa=csa_constr(test_file2,covgs, "int_alphabet_file","memory_log_file","csa_file",true);
+
+  test_file2="../test_cases/one_snp.txt";
+  query="ttacacagaactagagag";
+  mask_file="../test_cases/one_snp_mask_a.txt";
+  ifstream g(mask_file);
+  bool precalc=false;
+
+  int a;
+  mask_a.clear();
+  while (g>>a) mask_a.push_back(a);
+
+  csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,16777216> csa=csa_constr(test_file2,"int_alphabet_file","memory_log_file","csa_file",true);
 
   std::list<std::pair<uint64_t,uint64_t>> sa_intervals, sa_intervals_rev;
   std::list<std::vector<std::pair<uint32_t, std::vector<int>>>> sites;
   bool first_del=false;
 
   q=query;
-  for (int i=0;i<q.length();i++) {
+  for (uint16_t i=0;i<q.length();i++) {
        if (q[i]=='A' or q[i]=='a') p.push_back(1);
        if (q[i]=='C' or q[i]=='c') p.push_back(2);
        if (q[i]=='G' or q[i]=='g') p.push_back(3);
        if (q[i]=='T' or q[i]=='t') p.push_back(4);
   }
 
-  std::vector<uint8_t>::iterator res_it=bidir_search_bwd(csa,0,csa.size(),0,csa.size(),p.begin(),p.end(), sa_intervals,sa_intervals_rev,sites,mask_a,6,first_del);
+  std::vector<uint8_t>::iterator res_it=bidir_search_bwd(csa,0,csa.size(),0,csa.size(),p.begin(),p.end(), sa_intervals,sa_intervals_rev,sites,mask_a,6,first_del, precalc);
 
   uint64_t no_occ=(*sa_intervals.begin()).second-(*sa_intervals.begin()).first;
   EXPECT_EQ(true,first_del);
@@ -99,9 +111,9 @@ TEST(BackwardSearchTest, OneSNP){
   sa_intervals_rev.clear();
   sites.clear();
 
-  csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa_rev=csa_constr(test_file2,covgs, "int_alphabet_file","memory_log_file","csa_file",false);
+  csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,16777216> csa_rev=csa_constr(test_file2, "int_alphabet_file","memory_log_file","csa_file",false);
   first_del=false;
-  res_it=bidir_search_fwd(csa_rev,0,csa_rev.size(),0,csa_rev.size(),p.begin(),p.end(), sa_intervals,sa_intervals_rev,sites,mask_a,6,first_del);  
+  res_it=bidir_search_fwd(csa_rev,0,csa_rev.size(),0,csa_rev.size(),p.begin(),p.end(), sa_intervals,sa_intervals_rev,sites,mask_a,6,first_del,precalc);  
 
   no_occ=(*sa_intervals.begin()).second-(*sa_intervals.begin()).first;
   EXPECT_EQ(true,first_del);
@@ -114,7 +126,7 @@ TEST(BackwardSearchTest, OneSNP){
   p.clear();
 }
 
-
+/*
 TEST(BackwardSearchTest, TwoSNPs){
 
   csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,2> csa=csa_constr(test_file2,covgs, "int_alphabet_file","memory_log_file","csa_file",true);
