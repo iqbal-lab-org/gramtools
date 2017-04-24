e67439129f14f2d20e417ac7e7b695eba2682004
Robyn Ffrancon <rffrancon@gmail.com>
2017-04-24 14:41:15 +0100

    initial code reformatting

--- src/parse_masks.cpp
+++ src/parse_masks.cpp
@@ -10,50 +10,50 @@ using namespace sdsl;
 
 //need to add assertions
 
-uint64_t parse_masks(std::vector<uint64_t>& mask_s, std::vector<int>& mask_a, string sites_fname, string alleles_fname, std::vector<std::vector<int>>& covgs) {
-  int no_alleles,a;
-  uint64_t d,no_sites;
-  std::ifstream h1(sites_fname);
-  std::ifstream h2(alleles_fname);
+uint64_t parse_masks(std::vector<uint64_t> &mask_s, std::vector<int> &mask_a, string sites_fname, string alleles_fname,
+                     std::vector<std::vector<int>> &covgs) {
+    int no_alleles, a;
+    uint64_t d, no_sites;
+    std::ifstream h1(sites_fname);
+    std::ifstream h2(alleles_fname);
 
-  no_sites=0;
-  while (h1>>d)
-  { 
-	  if (d>no_sites) {
-		  no_sites=d;
-		  //		  mask_s.push_back(0); //fix bug in mask generation script that was missing the 0 from the beginning of each site
-	  }
-	  mask_s.push_back(d);
-  }
-  h1.close();
- 
-  no_alleles=0;
-  int i=0;
-  //assert at least 2 alleles at each site
-  while (h2>>a)
-    { 
-      if (a>no_alleles)
-	no_alleles=a;
-      if (a<no_alleles && a!=0) {
-	covgs.push_back(std::vector<int> (no_alleles,0));
- 	no_alleles=a;
-      }
-      i++;
-      mask_a.push_back(a);
+    no_sites = 0;
+    while (h1 >> d) {
+        if (d > no_sites) {
+            no_sites = d;
+            //		  mask_s.push_back(0); //fix bug in mask generation script that was missing the 0 from the beginning of each site
+        }
+        mask_s.push_back(d);
     }
-  h2.close();
+    h1.close();
 
-  if (no_alleles>0) {
-    covgs.push_back(std::vector<int> (no_alleles,0));
-  }
+    no_alleles = 0;
+    int i = 0;
+    //assert at least 2 alleles at each site
+    while (h2 >> a) {
+        if (a > no_alleles)
+            no_alleles = a;
+        if (a < no_alleles && a != 0) {
+            covgs.push_back(std::vector<int>(no_alleles, 0));
+            no_alleles = a;
+        }
+        i++;
+        mask_a.push_back(a);
+    }
+    h2.close();
 
-  /*  for (i=3141175;i<=3413540;i++) {
-    mask_s.push_back(0);
-    mask_a.push_back(0);
-    }*/
+    if (no_alleles > 0) {
+        covgs.push_back(std::vector<int>(no_alleles, 0));
+    }
 
-  cout<<endl<<mask_s.size()<<" "<<mask_a.size()<<endl;
+    /*  for (i=3141175;i<=3413540;i++) {
+      mask_s.push_back(0);
+      mask_a.push_back(0);
+      }*/
 
-  //  cout<<"Covgs dim"<<covgs.size()<<" "<<covgs.front().size()<<" "<<covgs.back().size()<<endl;
-  return(no_sites+1); //no_sites is last odd number in mask_sites, but alphabet size is the even number corresponding to it
+    cout << endl << mask_s.size() << " " << mask_a.size() << endl;
+
+    //  cout<<"Covgs dim"<<covgs.size()<<" "<<covgs.front().size()<<" "<<covgs.back().size()<<endl;
+    return (no_sites +
+            1); //no_sites is last odd number in mask_sites, but alphabet size is the even number corresponding to it
 }
