5f3d2b2f5cf00e743091aa456d39202160e8d20f
Robyn Ffrancon <rffrancon@gmail.com>
2017-04-27 01:55:58 +0100

    fix gitignore, correct files now tracked

---
+++ include/parse_masks.h
@@ -0,0 +1,25 @@
+#ifndef GRAMTOOLS_PARSE_MASKS_H
+#define GRAMTOOLS_PARSE_MASKS_H
+
+#include <vector>
+#include <string>
+#include <fstream>
+
+class MasksParser {
+public:
+    uint64_t max_alphabet_num;
+    std::vector<uint64_t> sites;
+
+    std::vector<int> allele;
+    std::vector<std::vector<int>> allele_coverage;
+
+    MasksParser(){};
+    MasksParser(const std::string &sites_fname, const std::string &alleles_fname);
+
+private:
+    void parse_sites(std::istream &stream);
+    void parse_allele(std::istream &stream);
+};
+
+
+#endif //GRAMTOOLS_PARSE_MASKS_H
