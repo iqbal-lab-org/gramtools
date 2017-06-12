201b044fde69c30c7d42a88cc5f9880a043023a9
Sorina Maciuca <sm0179@well.ox.ac.uk>
2017-06-12 22:07:03 +0100

    updated kmer precalculation in kmers.cpp with rank storage

--- src/kmers.cpp
+++ src/kmers.cpp
@@ -53,7 +53,7 @@ std::vector<std::string> split(std::string cad,std::string delim)
 void * worker (void *st)
 {
     thread_data* th=(thread_data *) st;
-    precalc_kmer_matches(*(th->csa),th->k,*(th->kmer_idx),*(th->kmer_idx_rev),*(th->kmer_sites),*(th->mask_a),th->maxx,*(th->kmers_in_ref),*(th->kmers));
+    precalc_kmer_matches(*(th->csa),th->k,*(th->kmer_idx),*(th->kmer_idx_rev),*(th->kmer_sites),*(th->mask_a),th->maxx,*(th->kmers_in_ref),*(th->kmers),*(th->rank_all));
     return NULL;
 }
 
@@ -68,7 +68,8 @@ void gen_precalc_kmers(
         std::vector<int> &mask_a,
         std::string kmer_fname,
         uint64_t maxx,
-        int k
+        int k,
+	std::unordered_map<uint8_t,std::vector<uint64_t>>& rank_all
 )
 {
 
@@ -112,6 +113,7 @@ void gen_precalc_kmers(
         td[i].maxx=maxx;
         td[i].kmers_in_ref=&kmers_in_ref[i];
         td[i].kmers=&kmers[i];
+	td[i].rank_all=&rank_all;
         pthread_create(&threads[i], NULL, worker, &td[i] );
         //worker(&td[i]);
     }
@@ -263,13 +265,13 @@ void read_precalc_kmers(std::string fil, sequence_map<std::vector<uint8_t>,
 
 KmersData get_kmers(csa_wt<wt_int<bit_vector,rank_support_v5<>>,2,16777216> &csa,
                    std::vector<int> &mask_a, std::string kmer_fname,
-                   uint64_t maxx, int k){
+		   uint64_t maxx, int k, std::unordered_map<uint8_t,std::vector<uint64_t>>& rank_all){
 
     if (!fexists(std::string(kmer_fname)+".precalc"))
     {
         std::cout << "Precalculated kmers not found, calculating them using "
                   << THREADS << " threads" << std::endl;
-        gen_precalc_kmers(csa, mask_a, kmer_fname, maxx, k);
+        gen_precalc_kmers(csa, mask_a, kmer_fname, maxx, k, rank_all);
         std::cout << "Finished precalculating kmers" << std::endl;
     }
 
