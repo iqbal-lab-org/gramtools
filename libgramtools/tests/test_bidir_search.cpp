d36cceea1ef7ae142e7df93194f94ef0a8ba48e3
Robyn Ffrancon <rffrancon@gmail.com>
2017-08-29 14:31:26 +0100

    expected sa_intervals explicitly stated

--- libgramtools/tests/test_bidir_search.cpp
+++ libgramtools/tests/test_bidir_search.cpp
@@ -38,6 +38,10 @@ protected:
         prg_fpath = "./prg_" + uuid_str;
     }
 
+    virtual void TearDown() {
+        std::remove(prg_fpath.c_str());
+    }
+
     FM_Index fm_index_from_raw_prg(const std::string &prg_raw) {
         std::vector<uint64_t> prg = encode_prg(prg_raw);
         dump_encoded_prg(prg, prg_fpath);
@@ -46,15 +50,12 @@ protected:
         return fm_index;
     }
 
-    virtual void TearDown() {
-        std::remove(prg_fpath.c_str());
-    }
-
 };
 
 
 TEST_F(BackwardSearchTest, NoVariants1) {
     const auto prg_raw = "a";
+    const uint64_t max_alphabet_num = 4;
     const std::string read = "a";
     std::vector<int> allele_mask = {0};
 
@@ -70,12 +71,12 @@ TEST_F(BackwardSearchTest, NoVariants1) {
 
     bidir_search_bwd(sa_intervals, sites, delete_first_interval,
                      encoded_read.begin(), encoded_read.end(),
-                     allele_mask, 4, kmer_index_generated, rank_all, fm_index);
+                     allele_mask, max_alphabet_num, kmer_index_generated, rank_all, fm_index);
 
-    uint64_t no_occ = (*sa_intervals.begin()).second - (*sa_intervals.begin()).first;
-    EXPECT_TRUE(!delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 1);
-    EXPECT_EQ(no_occ, 1);
+    EXPECT_FALSE(delete_first_interval);
+
+    const SA_Intervals expected_sa_intervals = {{1, 2}};
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {Site()};
     EXPECT_EQ(expected, sites);
@@ -85,6 +86,7 @@ TEST_F(BackwardSearchTest, NoVariants1) {
 TEST_F(BackwardSearchTest, OneSNP) {
     // aligns across SNP allele 1 (and both flanks)
     const auto prg_raw = "catttacaca5g6t5aactagagagca";
+    const uint64_t max_alphabet_num = 6;
     const auto read = "ttacacagaactagagag";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0,
@@ -94,22 +96,23 @@ TEST_F(BackwardSearchTest, OneSNP) {
     };
 
     const FM_Index fm_index = fm_index_from_raw_prg(prg_raw);
-    const DNA_Rank rank_all = calculate_ranks(fm_index);
+    const DNA_Rank &rank_all = calculate_ranks(fm_index);
     const auto encoded_read = encode_read(read);
-
-    SA_Intervals sa_intervals = {{0, fm_index.size()}};
-    Sites sites = {Site()};
 
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 6, kmer_index_generated, rank_all, fm_index);
+    SA_Intervals sa_intervals = {{0, fm_index.size()}};
+    Sites sites = {Site()};
 
-    uint64_t no_occ = (*sa_intervals.begin()).second - (*sa_intervals.begin()).first;
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated, rank_all, fm_index);
+
     EXPECT_TRUE(delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 1);
-    EXPECT_EQ(no_occ, 1);
+
+    const SA_Intervals expected_sa_intervals = {{22, 23}};
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {
             {VariantSite(5, {1})},
@@ -121,6 +124,7 @@ TEST_F(BackwardSearchTest, OneSNP) {
 TEST_F(BackwardSearchTest, TwoSNPs) {
     //aligns across both SNPs, both allele 1
     const auto prg_raw = "catttacaca5g6t5aactag7a8g7agcagggt";
+    const uint64_t max_alphabet_num = 8;
     const auto read = "ttacacagaactagaagcag";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -139,13 +143,15 @@ TEST_F(BackwardSearchTest, TwoSNPs) {
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 8, kmer_index_generated, rank_all, fm_index);
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
-    uint64_t no_occ = (*sa_intervals.begin()).second - (*sa_intervals.begin()).first;
     EXPECT_TRUE(delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 1);
-    EXPECT_EQ(no_occ, 1);
+
+    const SA_Intervals expected_sa_intervals = {{26, 27}};
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {
             {VariantSite(7, {1}), VariantSite(5, {1})},
@@ -157,6 +163,7 @@ TEST_F(BackwardSearchTest, TwoSNPs) {
 TEST_F(BackwardSearchTest, Two_matches_one_variable_one_nonvariable_region) {
     //one match crosses allele 1, and the other in nonvar
     const auto prg_raw = "catttacaca5g6t5aactagagagcaacagaactctct";
+    const uint64_t max_alphabet_num = 6;
     const auto read = "acagaac";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -175,16 +182,18 @@ TEST_F(BackwardSearchTest, Two_matches_one_variable_one_nonvariable_region) {
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 6, kmer_index_generated, rank_all, fm_index);
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
-    uint64_t no_occ = 0;
-    for (const auto &sa_interval: sa_intervals)
-        no_occ += sa_interval.second - sa_interval.first;
+    EXPECT_FALSE(delete_first_interval);
 
-    EXPECT_TRUE(!delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 2);
-    EXPECT_EQ(no_occ, 2);
+    const SA_Intervals expected_sa_intervals = {
+            {5, 6},
+            {6, 7},
+    };
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {
             {},
@@ -197,6 +206,7 @@ TEST_F(BackwardSearchTest, Two_matches_one_variable_one_nonvariable_region) {
 TEST_F(BackwardSearchTest, Two_matches_one_variable_second_allele_one_nonvariable_region) {
     //one match crosses allele 2, and the other in nonvar
     const auto prg_raw = "catttacaca5g6t5aactagagagcaacataactctct";
+    const uint64_t max_alphabet_num = 6;
     const auto read = "acataac";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -215,16 +225,18 @@ TEST_F(BackwardSearchTest, Two_matches_one_variable_second_allele_one_nonvariabl
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 6, kmer_index_generated, rank_all, fm_index);
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
-    uint64_t no_occ = 0;
-    for (const auto &sa_interval: sa_intervals)
-        no_occ += sa_interval.second - sa_interval.first;
+    EXPECT_FALSE(delete_first_interval);
 
-    EXPECT_TRUE(!delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 2);
-    EXPECT_EQ(no_occ, 2);
+    const SA_Intervals expected_sa_intervals = {
+            {5, 6},
+            {6, 7},
+    };
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {
             {},
@@ -237,6 +249,7 @@ TEST_F(BackwardSearchTest, Two_matches_one_variable_second_allele_one_nonvariabl
 TEST_F(BackwardSearchTest, Two_long_sites) {
     //read aligns from middle of  allele 3 of site 5 and allele 1 of site 7
     const auto prg_raw = "acgacacat5gatag6tagga6gctcg6gctct5gctcgatgactagatagatag7cga8cgc8tga8tgc7ggcaacatctacga";
+    const uint64_t max_alphabet_num = 8;
     const auto read = "gctcggctcgatgactagatagatagcgaggcaac";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -254,21 +267,20 @@ TEST_F(BackwardSearchTest, Two_long_sites) {
     const auto encoded_read = encode_read(read);
 
     SA_Intervals sa_intervals = {{0, fm_index.size()}};
-    Sites sites = { Site() };
+    Sites sites = {Site()};
 
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 8, kmer_index_generated, rank_all, fm_index);
-
-    uint64_t no_occ = 0;
-    for (const auto &sa_interval: sa_intervals)
-        no_occ += sa_interval.second - sa_interval.first;
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
     EXPECT_TRUE(delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 1);
-    EXPECT_EQ(no_occ, 1);
+
+    const SA_Intervals expected_sa_intervals = {{53, 54}};
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {
             {VariantSite(7, {1}), VariantSite(5, {})},
@@ -280,6 +292,7 @@ TEST_F(BackwardSearchTest, Two_long_sites) {
 TEST_F(BackwardSearchTest, Match_within_long_site_match_outside) {
     //read aligns in allele 2 of site 5, and in non-var region
     const auto prg_raw = "gacatagacacacagt5gtcgcctcgtcggctttgagt6gtcgctgctccacacagagact5ggtgctagac7c8a7tcagctgctccacacagaga";
+    const uint64_t max_alphabet_num = 8;
     const auto read = "ctgctccacacagaga";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -299,21 +312,20 @@ TEST_F(BackwardSearchTest, Match_within_long_site_match_outside) {
     const auto encoded_read = encode_read(read);
 
     SA_Intervals sa_intervals = {{0, fm_index.size()}};
-    Sites sites = { Site() };
+    Sites sites = {Site()};
 
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 8, kmer_index_generated, rank_all, fm_index);
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
-    uint64_t no_occ = 0;
-    for (const auto &sa_interval: sa_intervals)
-        no_occ += sa_interval.second - sa_interval.first;
+    EXPECT_FALSE(delete_first_interval);
 
-    EXPECT_TRUE(!delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 1);
-    EXPECT_EQ(no_occ, 2);
+    const SA_Intervals expected_sa_intervals = {{45, 47}};
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {Site()};
     EXPECT_EQ(expected, sites);
@@ -323,6 +335,7 @@ TEST_F(BackwardSearchTest, Match_within_long_site_match_outside) {
 TEST_F(BackwardSearchTest, Long_site_and_repeated_snp_on_edge_of_site) {
     //read aligns across sites 5 and 7, allele 1 in both cases
     const auto prg_raw = "gacatagacacacagt5gtcgcctcgtcggctttgagt6gtcgctgctccacacagagact5ggtgctagac7c8a7ccagctgctccacacagaga";
+    const uint64_t max_alphabet_num = 8;
     const auto read = "tagacacacagtgtcgcctcgtcggctttgagtggtgctagacccca";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -347,16 +360,15 @@ TEST_F(BackwardSearchTest, Long_site_and_repeated_snp_on_edge_of_site) {
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 8, kmer_index_generated, rank_all, fm_index);
-
-    uint64_t no_occ = 0;
-    for (const auto &sa_interval: sa_intervals)
-        no_occ += sa_interval.second - sa_interval.first;
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
     EXPECT_TRUE(delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 1);
-    EXPECT_EQ(no_occ, 1);
+
+    const SA_Intervals expected_sa_intervals = {{75, 76}};
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const Sites expected = {
             {VariantSite(7, {1}), VariantSite(5, {1})},
@@ -368,6 +380,7 @@ TEST_F(BackwardSearchTest, Long_site_and_repeated_snp_on_edge_of_site) {
 TEST_F(BackwardSearchTest, Multiple_matches_over_multiple_sites) {
     //read aligns over allele 1 of site 5, the nonvariableregion and allele 3 of site 7
     const auto prg_raw = "acgacacat5gatag6tagga6gctcg6gctct5gctcgtgataatgactagatagatag7cga8cgc8tga8tgc7taggcaacatctacga";
+    const uint64_t max_alphabet_num = 8;
     const auto read = "tgata";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -395,16 +408,19 @@ TEST_F(BackwardSearchTest, Multiple_matches_over_multiple_sites) {
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 8, kmer_index_generated, rank_all, fm_index);
-
-    uint64_t no_occ = 0;
-    for (const auto &sa_interval: sa_intervals)
-        no_occ += sa_interval.second - sa_interval.first;
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
     EXPECT_FALSE(delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 3);
-    EXPECT_EQ(no_occ, 3);
+
+    const SA_Intervals expected_sa_intervals = {
+            {79, 80},
+            {80, 81},
+            {82, 83},
+    };
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     // note this unit test allows for an implementation limitation
     // of gramtools right now - unless a read crosses an odd number, it is not stored in sites()
@@ -428,6 +444,7 @@ TEST_F(BackwardSearchTest, Multiple_matches_over_multiple_sites) {
 TEST_F(BackwardSearchTest, One_match_many_sites) {
     //overlaps site5-allele1, site7-allele2, site9-allele1, site11-allele1,  site13-allele2, site15-allele2
     const auto prg_raw = "agggccta5c6t5acatgatc7a8g7tgatca9c10a9cata11g12t11aggtcgct13c14g13ggtc15atc16cat15ttcg";
+    const uint64_t max_alphabet_num = 16;
     const auto read = "cctacacatgatcgtgatcaccatagaggtcgctgggtccat";
     const std::vector<int> allele_mask = {
             0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -451,16 +468,15 @@ TEST_F(BackwardSearchTest, One_match_many_sites) {
     bool delete_first_interval = false;
     const bool kmer_index_generated = false;
 
-    bidir_search_bwd(sa_intervals, sites, delete_first_interval, encoded_read.begin(),
-                     encoded_read.end(), allele_mask, 16, kmer_index_generated, rank_all, fm_index);
-
-    uint64_t no_occ = 0;
-    for (const auto &sa_interval: sa_intervals)
-        no_occ += sa_interval.second - sa_interval.first;
+    bidir_search_bwd(sa_intervals, sites, delete_first_interval,
+                     encoded_read.begin(), encoded_read.end(),
+                     allele_mask, max_alphabet_num, kmer_index_generated,
+                     rank_all, fm_index);
 
     EXPECT_TRUE(delete_first_interval);
-    EXPECT_EQ(sa_intervals.size(), 1);
-    EXPECT_EQ(no_occ, 1);
+
+    const SA_Intervals expected_sa_intervals = {{19, 20}};
+    EXPECT_EQ(sa_intervals, expected_sa_intervals);
 
     const auto &first_site = sites.front();
 
