0ffd8f67b04a932865de0775f68015d6cb1dd337
Robyn Ffrancon <rffrancon@gmail.com>
2017-09-11 12:00:39 +0100

    introduced PRG_Info data structure to organise all prg related data structures

--- libgramtools/include/prg.hpp
+++ libgramtools/include/prg.hpp
@@ -4,9 +4,21 @@
 #include <string>
 #include <tuple>
 
+#include "masks.hpp"
+#include "fm_index.hpp"
+#include "ranks.hpp"
+
 
 #ifndef GRAMTOOLS_PRG_HPP
 #define GRAMTOOLS_PRG_HPP
+
+struct PRG_Info {
+    FM_Index fm_index;
+    DNA_Rank dna_rank;
+    SitesMask sites_mask;
+    AlleleMask allele_mask;
+    uint64_t max_alphabet_num;
+};
 
 uint64_t max_alphabet_num(const std::string &prg_raw);
 
