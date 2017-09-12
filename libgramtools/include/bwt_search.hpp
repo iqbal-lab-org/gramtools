0c01f05d8bdaf22cde3cf1d9837114dd163dc93e
Robyn Ffrancon <rffrancon@gmail.com>
2017-09-12 11:09:19 +0100

    removed unused function decleration

--- libgramtools/include/bwt_search.hpp
+++ libgramtools/include/bwt_search.hpp
@@ -11,11 +11,8 @@
 #ifndef GRAMTOOLS_BWT_SEARCH_H
 #define GRAMTOOLS_BWT_SEARCH_H
 
-void precalc_ranks(const FM_Index &fm_index, const DNA_Rank &rank_all);
-
 SA_Interval bidir_search(const uint8_t next_char, const SA_Interval &sa_interval, const PRG_Info &prg_info);
 
 bool skip(uint64_t &left, uint64_t &right, const uint64_t num, const PRG_Info &prg_info);
-
 
 #endif //GRAMTOOLS_BWT_SEARCH_H
