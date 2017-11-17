850de6cbbccba18efdf5db6e3c06058acad7f207
martinghunt <martin.g.hunt@gmail.com>
2017-11-17 12:24:08 +0000

    Test when 0 or 1 SNPs in VCF

--- gramtools/tests/test_vcf_to_linear_prg_perl.py
+++ gramtools/tests/test_vcf_to_linear_prg_perl.py
@@ -1,12 +1,47 @@
+import filecmp
 import os
+import subprocess
 import unittest
 
 this_file = os.path.abspath(__file__)
 vcf_to_linear_prg_pl = os.path.abspath(os.path.join(this_file, os.pardir, os.pardir, 'utils', 'vcf_to_linear_prg.pl'))
+data_dir = os.path.abspath(os.path.join(this_file, os.pardir, 'data', 'vcf_to_linear_prg_perl'))
 
 class TestVcfToLinearPrgPerl(unittest.TestCase):
     def test_found_perl_script(self):
         '''Test that we found the script vcf_to_linear_prg.pl'''
         self.assertTrue(os.path.exists(vcf_to_linear_prg_pl))
 
+    # Every test follows the same pattern: run the script and
+    # check the output file is the same as the expected output
+    # file. The only thing changing here is the names of the files
+    def _test_one_run(self, file_prefix):
+        vcf_file = os.path.join(data_dir, file_prefix + '.vcf')
+        ref_file = os.path.join(data_dir, file_prefix + '.ref.fa')
+        expected_prg = os.path.join(data_dir, file_prefix + '.prg')
+        outfile = 'tmp.' + file_prefix + '.prg'
+        if os.path.exists(outfile):
+            os.unlink(outfile)
+        command = ' '.join([
+            'perl', vcf_to_linear_prg_pl,
+            '--vcf', vcf_file,
+            '--ref',  ref_file,
+            '--min_freq 0.01',
+            '--outfile', outfile,
+        ])
+        completed_process = subprocess.run(command, shell=True)
+        self.assertEqual(0, completed_process.returncode)
+        self.assertTrue(filecmp.cmp(expected_prg, outfile, shallow=False))
+        for suffix in ['', '.fa', '.mask_alleles', '.mask_sites', '.vcf']:
+            os.unlink(outfile + suffix)
+
+
+    def test_prg_with_no_variants(self):
+        '''Test make PRG when no variants in VCF'''
+        self._test_one_run('prg_with_no_variants')
+
+
+    def test_prg_with_one_snp(self):
+        '''Test make PRG when one SNP VCF'''
+        self._test_one_run('prg_with_one_snp')
 
