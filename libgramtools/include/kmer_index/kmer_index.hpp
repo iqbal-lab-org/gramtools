046aee5c2f45ef677253911deba741a74aa164fa
Robyn Ffrancon <rffrancon@gmail.com>
2018-02-15 16:28:22 +0000

    remove unused code

--- libgramtools/include/kmer_index/kmer_index.hpp
+++ libgramtools/include/kmer_index/kmer_index.hpp
@@ -12,8 +12,6 @@
 
 std::ostream &operator<<(std::ostream &os, const KmerIndexCache &cache);
 
-Base encode_dna_base(const char &base_str);
-
 std::string dump_kmer(const Pattern &kmer);
 
 std::string dump_sa_intervals(const SearchStates &search_states);
@@ -26,8 +24,6 @@ void dump_kmer_index(const KmerIndex &kmer_index, const Parameters &parameters);
 
 KmerIndex index_kmers(const Patterns &kmers, const int kmer_size, const PRG_Info &prg_info);
 
-inline bool file_exists(const std::string &name);
-
 static inline std::string &left_trim(std::string &s);
 
 static inline std::string &right_trim(std::string &s);
@@ -35,10 +31,6 @@ static inline std::string &right_trim(std::string &s);
 static inline std::string &trim(std::string &s);
 
 std::vector<std::string> split(const std::string &cad, const std::string &delim);
-
-bool parse_crosses_marker_flag(const std::string &in_reference_flag_str);
-
-Pattern parse_encoded_kmer(const std::string &encoded_kmer_str);
 
 std::vector<SA_Interval> parse_sa_intervals(const std::string &full_sa_intervals_str);
 
@@ -54,7 +46,5 @@ KmerIndex load_kmer_index(const Parameters &parameters);
 
 void generate_kmer_index(const Parameters &parameters,
                          const PRG_Info &prg_info);
-
-KmerIndex get_kmer_index(const std::string &kmer_fname, const int kmer_size, const PRG_Info &prg_info);
 
 #endif //GRAMTOOLS_KMER_INDEX_HPP
