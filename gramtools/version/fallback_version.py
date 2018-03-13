760e8ae5d5d637bc335f2d9c68e0faba6dfda5c7
Robyn Ffrancon <rffrancon@gmail.com>
2018-03-13 11:02:44 +0000

    fix: version timestamp changed to unix time

--- gramtools/version/fallback_version.py
+++ gramtools/version/fallback_version.py
@@ -1,4 +1,3 @@
 version_number = "0.5.0"
 last_git_commit_hash = 'NA'
-current_git_branch = 'NA'
 truncated_git_commits = 'NA'
--- gramtools/version/report.py
+++ gramtools/version/report.py
@@ -19,7 +19,6 @@ def report():
     report_dict = collections.OrderedDict([
         ('version_number', version.version_number),
         ('last_git_commit_hash', version.last_git_commit_hash),
-        ('current_git_branch', version.current_git_branch),
         ('truncated_git_commits', commits),
     ])
     report_json = json.dumps(report_dict, indent=4)
