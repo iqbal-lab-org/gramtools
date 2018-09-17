f12a008dce6db8d71730a4eed0987b6840621450
Robyn Ffrancon <robyn.ffrancon@protonmail.com>
2018-09-17 16:13:37 +0100

    added unit tests for loading kmer index where index contains two kmers and multiple search states

--- libgramtools/tests/kmer_index/test_load.cpp
+++ libgramtools/tests/kmer_index/test_load.cpp
@@ -175,3 +175,61 @@ TEST(ParseKmerIndex, GivenSingleKmerWithTwoSearchStates_CorrectKmerIndex) {
     };
     EXPECT_EQ(result, expected);
 }
+
+
+TEST(ParseKmerIndex, GivenTwoKmersWithMultipleSearchStates_CorrectKmerIndex) {
+    Parameters parameters = {};
+    parameters.kmers_size = 4;
+
+    parameters.kmers_fpath = "@kmers_fpath";
+    sdsl::int_vector<3> all_kmers = {2, 2, 2, 2, 4, 4, 4, 4};
+    sdsl::store_to_file(all_kmers, parameters.kmers_fpath);
+
+    parameters.kmers_stats_fpath = "@kmers_stats_fpath";
+    sdsl::int_vector<> kmers_stats = {1, 1, 2, 1, 2};
+    sdsl::util::bit_compress(kmers_stats);
+    sdsl::store_to_file(kmers_stats, parameters.kmers_stats_fpath);
+
+    parameters.sa_intervals_fpath = "@sa_intervals_fpath";
+    sdsl::int_vector<> sa_intervals = {1, 1, 1, 1, 2, 2};
+    sdsl::util::bit_compress(sa_intervals);
+    sdsl::store_to_file(sa_intervals, parameters.sa_intervals_fpath);
+
+    parameters.paths_fpath = "@paths_fpath";
+    sdsl::int_vector<> paths = {42, 43, 42, 43, 52, 53, 62, 63};
+    sdsl::util::bit_compress(paths);
+    sdsl::store_to_file(paths, parameters.paths_fpath);
+
+    auto result = kmer_index::load(parameters);
+
+    KmerIndex expected = {
+            {{2, 2, 2, 2},
+                    SearchStates {
+                            SearchState {
+                                    SA_Interval {1, 1},
+                                    VariantSitePath {
+                                            VariantSite {42, 43}
+                                    }
+                            }
+                    }
+            },
+            {{4, 4, 4, 4},
+                    SearchStates {
+                            SearchState {
+                                    SA_Interval {1, 1},
+                                    VariantSitePath {
+                                            VariantSite {42, 43}
+                                    }
+                            },
+                            SearchState {
+                                    SA_Interval {2, 2},
+                                    VariantSitePath {
+                                            VariantSite {52, 53},
+                                            VariantSite {62, 63}
+                                    }
+                            }
+                    }
+            }
+    };
+    EXPECT_EQ(result, expected);
+}
