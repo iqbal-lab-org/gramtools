4ba1431fda352efb3e3b27473d3d8d76df63347f
bricoletc <bletcher@ebi.ac.uk>
2019-04-12 15:37:05 +0100

    Make sdsl build portable- removes SSE4.2 and AVX2 instructions
    Remove `-march=native` and `-msse4.2` compiler flags in gramtools cmake
    file; the former especially reduces portability.

--- libgramtools/lib/sdsl.cmake
+++ libgramtools/lib/sdsl.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(sdsl
         DOWNLOAD_COMMAND  wget https://github.com/simongog/sdsl-lite/releases/download/v2.1.1/sdsl-lite-2.1.1.tar.gz.offline.install.gz --timestamping
         DOWNLOAD_DIR      "${CMAKE_CURRENT_BINARY_DIR}/download"
         CONFIGURE_COMMAND ""
-        BUILD_COMMAND     bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/src/sdsl-lite-2.1.1 && ./install.sh ${CMAKE_CURRENT_BINARY_DIR}"
+        BUILD_COMMAND     bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/src/sdsl-lite-2.1.1 && BUILD_PORTABLE=1 ./install.sh ${CMAKE_CURRENT_BINARY_DIR}"
         INSTALL_COMMAND   ""
         TEST_COMMAND      "")
 
