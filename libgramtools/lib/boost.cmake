79162b1adbf1e058fa15843ba3badba25812aa94
bricoletc <bletcher@ebi.ac.uk>
2019-12-10 12:20:48 +0000

    Silence boost build and restructure CMakeLists

--- libgramtools/lib/boost.cmake
+++ libgramtools/lib/boost.cmake
@@ -1,14 +1,18 @@
-execute_process(COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/src/boost_1_65_1)
+set(BOOST_SRC_DIR ${CMAKE_CURRENT_BINARY_DIR}/src/boost_1_65_1)
+
+execute_process(COMMAND mkdir -p ${BOOST_SRC_DIR})
 
 ExternalProject_Add(boost
         DOWNLOAD_COMMAND  wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz --timestamping
         DOWNLOAD_DIR      "${CMAKE_CURRENT_BINARY_DIR}/download"
-        CONFIGURE_COMMAND bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/src/boost_1_65_1 && ./bootstrap.sh --with-libraries=random,program_options,timer,system,filesystem,serialization --prefix=${CMAKE_CURRENT_BINARY_DIR}"
-        BUILD_COMMAND     bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/src/boost_1_65_1 && ./bjam install link=static"
+        CONFIGURE_COMMAND bash -c "cd ${BOOST_SRC_DIR} && ./bootstrap.sh \
+                        --with-libraries=random,program_options,timer,system,filesystem,serialization \
+                        --prefix=${BOOST_SRC_DIR} 1>/dev/null"
+        BUILD_COMMAND     bash -c "cd ${BOOST_SRC_DIR} && ./bjam install link=static 1>/dev/null"
         INSTALL_COMMAND   ""
         TEST_COMMAND      "")
 
 ExternalProject_Add_Step(boost extract_tar
-        COMMAND tar -xvzf ${CMAKE_CURRENT_BINARY_DIR}/download/boost_1_65_1.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR}/src
+        COMMAND tar -xzf ${CMAKE_CURRENT_BINARY_DIR}/download/boost_1_65_1.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR}/src
         DEPENDEES download
         DEPENDERS configure)
