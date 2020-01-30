a4e5d8d28a3be3e629c712c4b141d26a10c59faa
bricoletc <bletcher@ebi.ac.uk>
2020-01-30 09:35:26 +0000

    Refactored genotyping unit tests

--- libgramtools/submods/tests/genotype/infer/test_infer.cpp
+++ libgramtools/submods/tests/genotype/infer/test_infer.cpp
@@ -1,254 +1,138 @@
-/**
- * Tests full-round single-site genotyping:
- * - i. LevelGenotyper based one-site genotyping
+/** @file
+ * High-level `infer` command tests:
+ *  builds a coverage graph & gram index, maps reads to it, calls a genotyper, and tests the results!!
  */
+
 #include "gtest/gtest.h"
+#include "tests/common.hpp"
+#include "genotype/quasimap/quasimap.hpp"
 #include "genotype/infer/infer.hpp"
-#include "mocks.hpp"
 
 /**
- * LevelGenotyperModel tests (i.)
+ * Test the following Genotyping approach: Level Genotyping (LG)
  */
-TEST(TestLevelGenotyperModel_Failure, GivenOneAlleleOnly_Breaks){
-    // No likelihood ratio if only one allele. Note this should not present itself if allele extraction
-    // works correctly, as any bubble has at least 2 alleles.
-    allele_vector alleles{
-        Allele{"ACGT", {1, 1, 1, 1}, 0}
-    };
-    GroupedAlleleCounts gp_counts;
-    likelihood_related_stats l_stats;
-    EXPECT_DEATH(LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false), "");
+TEST(LevelGenotyping, Given2SiteNonNestedPRG_CorrectGenotypes){
+    std::string prg{"AATAA5C6G6AA7C8G8AA"};
+    Sequences kmers{encode_dna_bases("AA")};
+    prg_setup setup;
+    setup.setup_numbered_prg(prg, kmers);
+
+    allele_vector gt_alleles;
+    GenomicRead_vector reads;
+    // Multiple reads going through 5:1 and 7:1
+    for (int i = 0; i < 5; i++) reads.push_back(GenomicRead("Read", "AATAACAACAA", "???????????"));
+
+    // One read going through 5:2 and 7:1
+    reads.push_back(GenomicRead("ErrorRead", "AATAAGAACAA", "???????????"));
+
+    setup.quasimap_reads(reads);
+
+    LevelGenotyper genotyper(setup.prg_info.coverage_graph, setup.coverage.grouped_allele_counts,
+                             setup.read_stats, Ploidy::Haploid);
+    auto gt_recs = genotyper.get_genotyped_records();
+
+    gt_alleles = gt_recs.at(siteID_to_index(5))->get_unique_genotyped_alleles();
+    allele_vector expected_alleles{ Allele{"C", {5}, 0} };
+    EXPECT_EQ(gt_alleles, expected_alleles);
+
+    gt_alleles = gt_recs.at(0)->get_unique_genotyped_alleles();
+    expected_alleles = allele_vector{ Allele{"C", {5}, 0} };
+    EXPECT_EQ(gt_alleles, expected_alleles);
 }
 
-class TestLevelGenotyperModel_NullGTs : public ::testing::Test {
-protected:
-    allele_vector alleles{
-            Allele{ "A", {0}, 0 },
-            Allele{ "G", {0}, 1 },
-    };
+TEST(LevelGenotyping, Given2SiteNestedPRG_CorrectGenotypes){
+    std::string prg{"AATAA[CCC[A,G],T]AA"};
+    Sequences kmers{encode_dna_bases("AA")};
+    prg_setup setup;
+    setup.setup_bracketed_prg(prg, kmers);
 
-    GroupedAlleleCounts gp_counts{};
-    double mean_cov_depth{15}, mean_pb_error{0.01};
-    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
-};
+    allele_vector gt_alleles;
+    GenomicRead_vector reads;
+    // Multiple reads going through first allele of each site
+    for (int i = 0; i < 5; i++) reads.push_back(GenomicRead("Read", "AATAACCCGAA", "???????????"));
+    // One read going through second allele of site 1 and first allele of site 2
+    reads.push_back(GenomicRead("ErrorRead", "AATAATAA", "????????"));
 
-TEST_F(TestLevelGenotyperModel_NullGTs , Given0MeanCoverage_ReturnsNullGenotypedSite) {
-    l_stats.mean_cov_depth = 0;
-    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
+    setup.quasimap_reads(reads);
 
-    EXPECT_TRUE(genotyped.get_site()->is_null());
-}
+    LevelGenotyper genotyper(setup.prg_info.coverage_graph, setup.coverage.grouped_allele_counts,
+                             setup.read_stats, Ploidy::Haploid);
+    auto gt_recs = genotyper.get_genotyped_records();
 
-TEST_F(TestLevelGenotyperModel_NullGTs , GivenNoCoverageOnAllAlleles_ReturnsNullGenotypedSite) {
-    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
-
-    EXPECT_TRUE(genotyped.get_site()->is_null());
-}
-
-
-class TestLevelGenotyperModel_TwoAllelesWithCoverage : public ::testing::Test {
-protected:
-    allele_vector alleles{
-        Allele{ "ATCACC", {0, 0, 1, 1, 2, 2}, 0 },
-        Allele{ "ATGACC", {0, 0, 0, 0, 1, 1}, 0 },
-        Allele{ "GGGCC", {10, 12, 12, 14, 14}, 1 },
-    };
-
-    GroupedAlleleCounts gp_counts{
-            { {0}, 1 },
-            { {0, 1}, 1 }, // The allele sequences support one read being able to map like this
-            { {1}, 13 },
-    };
-
-    double mean_cov_depth{15}, mean_pb_error{0.01};
-    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
-};
-
-
-TEST_F(TestLevelGenotyperModel_TwoAllelesWithCoverage, GivenCoverage_ReturnsCorrectHaploidCall) {
-    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
-
-    auto genotyped_alleles = genotyped.get_site()->get_alleles();
+    gt_alleles = gt_recs.at(1)->get_unique_genotyped_alleles();
     allele_vector expected_alleles{
-            alleles.at(0), // REF is not called, but still makes it in here
-            alleles.at(2),
+            Allele{"G", {5}, 1}
     };
-    EXPECT_EQ(genotyped_alleles, expected_alleles);
+    EXPECT_EQ(gt_alleles, expected_alleles);
 
-    auto gtype = genotyped.get_site()->get_genotype();
-    EXPECT_TRUE(std::holds_alternative<GtypedIndices>(gtype));
-
-    // The genotype needs to get rescaled: it is index 2 in original allele vector, but 1 in retained alleles
-    GtypedIndices expected_gtype{1};
-    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
+    gt_alleles = gt_recs.at(0)->get_unique_genotyped_alleles();
+    expected_alleles = allele_vector{
+            Allele{"CCCG", {5, 5, 5, 5}, 0}
+    };
+    EXPECT_EQ(gt_alleles, expected_alleles);
 }
 
 
-TEST_F(TestLevelGenotyperModel_TwoAllelesWithCoverage, GivenCoverage_ReturnsCorrectDiploidCall) {
-    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
-
-    auto gtype = genotyped.get_site()->get_genotype();
-    EXPECT_TRUE(std::holds_alternative<GtypedIndices>(gtype));
-
-    GtypedIndices expected_gtype{1, 1};
-    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
-}
-
-TEST(TestLevelGenotyperModel_MinosParallel, GivenCoverages_CorrectGenotype){
-    // Note : comparing with Minos v0.9.1 commit@7c68ad0 (and with the hom likelihood not halved),
-    // which has the same test as this, I get the same likelihoods for the three genotypes
-    double mean_cov_depth{20}, mean_pb_error{0.01};
-
-    allele_vector alleles{
-            Allele{ "AA", {0, 1}, 0 },
-            Allele{ "TT", {20, 19}, 1 },
-    };
-
-    GroupedAlleleCounts gp_counts{
-            { {0}, 2 },
-            { {0, 1}, 1 },
-            { {1}, 20 },
-    };
-
-    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
-
-    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
-    auto gtype = genotyped.get_site()->get_genotype();
-    GtypedIndices expected_gtype{1, 1};
-    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
-}
-
-
-class TestLevelGenotyperModel_FourAlleles : public ::testing::Test{
-   // Simulating a case where each haplogroup has a bubble nested inside it,
-   // and those bubbles have been typed heterozygous.
+class LG_SnpsNestedInTwoHaplotypes : public ::testing::Test {
 protected:
-    allele_vector alleles{
-            Allele{ "AATAA", {8, 8, 8, 8, 8}, 0 },
-            Allele{ "AAGAA", {7, 7, 7, 7, 7}, 0 },
-            Allele{ "GGTGG", {15, 15, 15, 16, 16}, 1 }, // 15 unique + 1 common with next allele
-            Allele{ "GGCGG", {14, 14, 14, 15, 15}, 1 }, // 14 unique + 1 common with previous allele
-    };
+    void SetUp(){
+        std::string _prg{"ATCGGC[TC[A,G]TC,GG[T,G]GG]AT"};
+        auto all_kmers = gram::generate_all_kmers(2);
+        Sequences all_kmers_vector{all_kmers.begin(), all_kmers.end()};
+        setup.setup_bracketed_prg(_prg, all_kmers_vector);
 
-    GroupedAlleleCounts gp_counts{
-            { {0}, 15 },
-            { {1}, 30 },
-    };
-    double mean_cov_depth{30}, mean_pb_error{0.01};
-    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
-};
+        // This read goes through 5:1 and 7:2
+        for (int num_mapped{0}; num_mapped < 7; num_mapped++) reads.push_back(
+                    GenomicRead("Read1", "ATCGGCTCGTCAT", ".............") );
 
-TEST_F(TestLevelGenotyperModel_FourAlleles, GivenHaploGroup1SupportingMeanCov_CorrectGenotype){
-
-    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
-
-    auto genotyped_alleles = genotyped.get_site()->get_alleles();
-    allele_vector expected_alleles{
-            alleles.at(0), // REF is not called, but still makes it in here
-            alleles.at(2),
-            alleles.at(3),
-    };
-    EXPECT_EQ(genotyped_alleles, expected_alleles);
-
-    auto gtype = genotyped.get_site()->get_genotype();
-    GtypedIndices expected_gtype{1, 2};
-    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
-}
-
-TEST_F(TestLevelGenotyperModel_FourAlleles, GivenDifferentPloidies_CorrectNumberOfProducedGenotypes) {
-    auto haploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
-    EXPECT_EQ(haploid_genotyped.get_likelihoods().size(), 4);
-
-    auto diploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
-    // Expected number of genotypes: 4 diploid homozygous + (4 choose 2) diploid heterozygous
-    EXPECT_EQ(diploid_genotyped.get_likelihoods().size(), 10);
-}
-
-TEST(TestLevelGenotyperModel_IgnoredREF, GivenSeveralAllelesAndIgnoredREF_CorrectNumberOfLikelihoods){
-    // Note this is only relevant to genotyping a site which contains a nested site
-    // Here we imagine we called one allele in each of haplogroups 0 and 1, and that a REF got prepended
-    allele_vector alleles{
-            Allele{ "A", {0}, 0 },
-            Allele{ "C", {8}, 0 },
-            Allele{ "G", {8}, 1 },
-    };
-
-    GroupedAlleleCounts gp_counts{
-            { {0}, 8 },
-            { {1}, 8 },
-    };
-    double mean_cov_depth{8}, mean_pb_error{0.01};
-    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
-
-    // The last param to LevelGenotyperModel is whether to avoid using the REF
-    auto haploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, true);
-    EXPECT_EQ(haploid_genotyped.get_likelihoods().size(), 2);
-
-    auto diploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, true);
-    // Two homs and one het. Note this only works if you have singleton coverage on each haplogroup
-    EXPECT_EQ(diploid_genotyped.get_likelihoods().size(), 3);
-}
-
-
-/**
- * LevelGenotyper tests (i.)
- */
-
-TEST(LevelGenotyperInvalidation, GivenChildMapAndCandidateHaplos_CorrectHaplosWithSites){
-    // site 7 lives on haplogroup 0 of site 5, and sites 9 and 11 live on its haplogroup 1.
-    parental_map par_map{
-            {7, VariantLocus{5, 1}},
-            {9, VariantLocus{5, 2}},
-            {11, VariantLocus{5, 2}},
-    };
-    child_map child_m = build_child_map(par_map);
-    LevelGenotyper g{child_m, gt_sites{}};
-
-    AlleleIds expected_haplogroups{0, 1}; // Expected in 0-based
-    auto haplos_with_sites = g.get_haplogroups_with_sites(5, {0, 1, 2, 3});
-    EXPECT_EQ(haplos_with_sites, expected_haplogroups);
-
-    auto empty_query = g.get_haplogroups_with_sites(7, {0, 1, 2, 3});
-    EXPECT_EQ(empty_query, AlleleIds{});
-}
-
-using ::testing::InSequence;
-using ::testing::Return;
-TEST(LevelGenotyperInvalidation, GivenNestingStructure_CorrectGenotypeNullifying){
-    parental_map par_map{
-            {7, VariantLocus{5, 1}},
-            {9, VariantLocus{7, 2}},
-    };
-    child_map child_m = build_child_map(par_map);
-
-    gt_sites sites(3);
-    auto site1 = std::make_shared<MockGenotypedSite>();
-    EXPECT_CALL(*site1, make_null())
-            .Times(1);
-    EXPECT_CALL(*site1, is_null())
-            .WillOnce(Return(false));
-    site1->set_num_haplogroups(5);
-    sites.at(1) = site1;
-
-    // SiteID 9 will get nulled by site 7.
-    // Then when site 5 nulls site 7, I want site 9 to signal it is already nulled.
-    auto site2 = std::make_shared<MockGenotypedSite>();
-    {
-        InSequence seq;
-        EXPECT_CALL(*site2, is_null())
-            .WillOnce(Return(false));
-        EXPECT_CALL(*site2, make_null())
-                .Times(1);
-        EXPECT_CALL(*site2, is_null())
-                .WillOnce(Return(true));
+        // This read goes through 5:2 and 9:2
+        reads.push_back( GenomicRead("Read2", "ATCGGCGGG", ".........") );
     }
-    site2->set_num_haplogroups(5);
-    sites.at(2) = site2;
 
-    LevelGenotyper g{child_m, sites};
+    void MapReadsAndHaploidGenotype(){
+        setup.quasimap_reads(reads);
+        LevelGenotyper genotyper(setup.prg_info.coverage_graph, setup.coverage.grouped_allele_counts,
+                                 setup.read_stats, Ploidy::Haploid);
+        gt_recs = genotyper.get_genotyped_records();
+    }
 
-    // I want site 9 to be invalidated in this call
-    g.invalidate_if_needed(7, AlleleIds{1});
-    // And this call to invalidate site 7, without attempting to invalidate site 9 which was already so by above call.
-    g.invalidate_if_needed(5, AlleleIds{0});
+    GenomicRead_vector reads;
+    allele_vector gt_alleles;
+    allele_vector expected_alleles;
+    gt_sites gt_recs;
+    prg_setup setup;
+};
+
+TEST_F(LG_SnpsNestedInTwoHaplotypes, MapNoReads_AllGenotypesAreNull){
+    LevelGenotyper genotyper(setup.prg_info.coverage_graph, setup.coverage.grouped_allele_counts,
+                             setup.read_stats, Ploidy::Haploid);
+    gt_recs = genotyper.get_genotyped_records();
+
+    for (auto const& gt_rec : gt_recs){
+        EXPECT_TRUE(gt_rec->is_null());
+    }
 }
+
+TEST_F(LG_SnpsNestedInTwoHaplotypes, MapReads_CorrectlyGenotypedSites){
+    MapReadsAndHaploidGenotype();
+
+    gt_alleles = gt_recs.at(siteID_to_index(5))->get_unique_genotyped_alleles();
+    expected_alleles = allele_vector{
+            Allele{"TCGTC", {7, 7, 7, 7, 7}, 0}
+    };
+    EXPECT_EQ(gt_alleles, expected_alleles);
+
+    gt_alleles = gt_recs.at(siteID_to_index(7))->get_unique_genotyped_alleles();
+    expected_alleles = allele_vector{
+            Allele{"G", {7}, 1}
+    };
+    EXPECT_EQ(gt_alleles, expected_alleles);
+}
+
+TEST_F(LG_SnpsNestedInTwoHaplotypes, MapReads_CorrectlyInvalidatedSites){
+    // Since we called 5:1, we should invalidate whatever lives on 5:2; which is site ID 9.
+    MapReadsAndHaploidGenotype();
+
+    EXPECT_TRUE(gt_recs.at(siteID_to_index(9))->is_null());
+}
+
--- libgramtools/submods/tests/genotype/infer/test_level_genotyper.cpp
+++ libgramtools/submods/tests/genotype/infer/test_level_genotyper.cpp
@@ -1,8 +1,9 @@
 /**
- * Tests the internals of LevelGenotyperModel
+ * Tests the internals of LevelGenotyperModel and of LevelGenotyper
  */
 #include "gtest/gtest.h"
-#include "genotype/infer/genotyping_models.hpp"
+#include "mocks.hpp"
+#include "genotype/infer/infer.hpp"
 
 using namespace gram::genotype::infer;
 
@@ -203,4 +204,252 @@ TEST(RescaleGenotypes, GivenVariousGenotypes_CorrectRescaling){
     GtypedIndices shuffled_order{4, 2};
     GtypedIndices  shuffled_order_rescaled{1, 2};
     EXPECT_EQ(g.rescale_genotypes( shuffled_order ), shuffled_order_rescaled);
+}
+
+
+/**
+ * Full run of the genotyping model
+ */
+TEST(TestLevelGenotyperModel_Failure, GivenOneAlleleOnly_Breaks){
+    // No likelihood ratio if only one allele. Note this should not present itself if allele extraction
+    // works correctly, as any bubble has at least 2 alleles.
+    allele_vector alleles{
+            Allele{"ACGT", {1, 1, 1, 1}, 0}
+    };
+    GroupedAlleleCounts gp_counts;
+    likelihood_related_stats l_stats;
+    EXPECT_DEATH(LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false), "");
+}
+
+class TestLevelGenotyperModel_NullGTs : public ::testing::Test {
+protected:
+    allele_vector alleles{
+            Allele{ "A", {0}, 0 },
+            Allele{ "G", {0}, 1 },
+    };
+
+    GroupedAlleleCounts gp_counts{};
+    double mean_cov_depth{15}, mean_pb_error{0.01};
+    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
+};
+
+TEST_F(TestLevelGenotyperModel_NullGTs , Given0MeanCoverage_ReturnsNullGenotypedSite) {
+    l_stats.mean_cov_depth = 0;
+    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
+
+    EXPECT_TRUE(genotyped.get_site()->is_null());
+}
+
+TEST_F(TestLevelGenotyperModel_NullGTs , GivenNoCoverageOnAllAlleles_ReturnsNullGenotypedSite) {
+    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
+
+    EXPECT_TRUE(genotyped.get_site()->is_null());
+}
+
+
+class TestLevelGenotyperModel_TwoAllelesWithCoverage : public ::testing::Test {
+protected:
+    allele_vector alleles{
+            Allele{ "ATCACC", {0, 0, 1, 1, 2, 2}, 0 },
+            Allele{ "ATGACC", {0, 0, 0, 0, 1, 1}, 0 },
+            Allele{ "GGGCC", {10, 12, 12, 14, 14}, 1 },
+    };
+
+    GroupedAlleleCounts gp_counts{
+            { {0}, 1 },
+            { {0, 1}, 1 }, // The allele sequences support one read being able to map like this
+            { {1}, 13 },
+    };
+
+    double mean_cov_depth{15}, mean_pb_error{0.01};
+    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
+};
+
+
+TEST_F(TestLevelGenotyperModel_TwoAllelesWithCoverage, GivenCoverage_ReturnsCorrectHaploidCall) {
+    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
+
+    auto genotyped_alleles = genotyped.get_site()->get_alleles();
+    allele_vector expected_alleles{
+            alleles.at(0), // REF is not called, but still makes it in here
+            alleles.at(2),
+    };
+    EXPECT_EQ(genotyped_alleles, expected_alleles);
+
+    auto gtype = genotyped.get_site()->get_genotype();
+    EXPECT_TRUE(std::holds_alternative<GtypedIndices>(gtype));
+
+    // The genotype needs to get rescaled: it is index 2 in original allele vector, but 1 in retained alleles
+    GtypedIndices expected_gtype{1};
+    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
+}
+
+
+TEST_F(TestLevelGenotyperModel_TwoAllelesWithCoverage, GivenCoverage_ReturnsCorrectDiploidCall) {
+    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
+
+    auto gtype = genotyped.get_site()->get_genotype();
+    EXPECT_TRUE(std::holds_alternative<GtypedIndices>(gtype));
+
+    GtypedIndices expected_gtype{1, 1};
+    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
+}
+
+TEST(TestLevelGenotyperModel_MinosParallel, GivenCoverages_CorrectGenotype){
+    // Note : comparing with Minos v0.9.1 commit@7c68ad0 (and with the hom likelihood not halved),
+    // which has the same test as this, I get the same likelihoods for the three genotypes
+    double mean_cov_depth{20}, mean_pb_error{0.01};
+
+    allele_vector alleles{
+            Allele{ "AA", {0, 1}, 0 },
+            Allele{ "TT", {20, 19}, 1 },
+    };
+
+    GroupedAlleleCounts gp_counts{
+            { {0}, 2 },
+            { {0, 1}, 1 },
+            { {1}, 20 },
+    };
+
+    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
+
+    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
+    auto gtype = genotyped.get_site()->get_genotype();
+    GtypedIndices expected_gtype{1, 1};
+    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
+}
+
+
+class TestLevelGenotyperModel_FourAlleles : public ::testing::Test{
+    // Simulating a case where each haplogroup has a bubble nested inside it,
+    // and those bubbles have been typed heterozygous.
+protected:
+    allele_vector alleles{
+            Allele{ "AATAA", {8, 8, 8, 8, 8}, 0 },
+            Allele{ "AAGAA", {7, 7, 7, 7, 7}, 0 },
+            Allele{ "GGTGG", {15, 15, 15, 16, 16}, 1 }, // 15 unique + 1 common with next allele
+            Allele{ "GGCGG", {14, 14, 14, 15, 15}, 1 }, // 14 unique + 1 common with previous allele
+    };
+
+    GroupedAlleleCounts gp_counts{
+            { {0}, 15 },
+            { {1}, 30 },
+    };
+    double mean_cov_depth{30}, mean_pb_error{0.01};
+    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
+};
+
+TEST_F(TestLevelGenotyperModel_FourAlleles, GivenHaploGroup1SupportingMeanCov_CorrectGenotype){
+
+    auto genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
+
+    auto genotyped_alleles = genotyped.get_site()->get_alleles();
+    allele_vector expected_alleles{
+            alleles.at(0), // REF is not called, but still makes it in here
+            alleles.at(2),
+            alleles.at(3),
+    };
+    EXPECT_EQ(genotyped_alleles, expected_alleles);
+
+    auto gtype = genotyped.get_site()->get_genotype();
+    GtypedIndices expected_gtype{1, 2};
+    EXPECT_EQ(std::get<GtypedIndices>(gtype), expected_gtype);
+}
+
+TEST_F(TestLevelGenotyperModel_FourAlleles, GivenDifferentPloidies_CorrectNumberOfProducedGenotypes) {
+    auto haploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, false);
+    EXPECT_EQ(haploid_genotyped.get_likelihoods().size(), 4);
+
+    auto diploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, false);
+    // Expected number of genotypes: 4 diploid homozygous + (4 choose 2) diploid heterozygous
+    EXPECT_EQ(diploid_genotyped.get_likelihoods().size(), 10);
+}
+
+TEST(TestLevelGenotyperModel_IgnoredREF, GivenSeveralAllelesAndIgnoredREF_CorrectNumberOfLikelihoods){
+    // Note this is only relevant to genotyping a site which contains a nested site
+    // Here we imagine we called one allele in each of haplogroups 0 and 1, and that a REF got prepended
+    allele_vector alleles{
+            Allele{ "A", {0}, 0 },
+            Allele{ "C", {8}, 0 },
+            Allele{ "G", {8}, 1 },
+    };
+
+    GroupedAlleleCounts gp_counts{
+            { {0}, 8 },
+            { {1}, 8 },
+    };
+    double mean_cov_depth{8}, mean_pb_error{0.01};
+    likelihood_related_stats l_stats = LevelGenotyper::make_l_stats(mean_cov_depth, mean_pb_error);
+
+    // The last param to LevelGenotyperModel is whether to avoid using the REF
+    auto haploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Haploid, &l_stats, true);
+    EXPECT_EQ(haploid_genotyped.get_likelihoods().size(), 2);
+
+    auto diploid_genotyped = LevelGenotyperModel(&alleles, &gp_counts, Ploidy::Diploid, &l_stats, true);
+    // Two homs and one het. Note this only works if you have singleton coverage on each haplogroup
+    EXPECT_EQ(diploid_genotyped.get_likelihoods().size(), 3);
+}
+
+
+/**
+ * Test LevelGenotyper internals
+ */
+
+TEST(LevelGenotyperInvalidation, GivenChildMapAndCandidateHaplos_CorrectHaplosWithSites){
+    // site 7 lives on haplogroup 0 of site 5, and sites 9 and 11 live on its haplogroup 1.
+    parental_map par_map{
+            {7, VariantLocus{5, 1}},
+            {9, VariantLocus{5, 2}},
+            {11, VariantLocus{5, 2}},
+    };
+    child_map child_m = build_child_map(par_map);
+    LevelGenotyper g{child_m, gt_sites{}};
+
+    AlleleIds expected_haplogroups{0, 1}; // Expected in 0-based
+    auto haplos_with_sites = g.get_haplogroups_with_sites(5, {0, 1, 2, 3});
+    EXPECT_EQ(haplos_with_sites, expected_haplogroups);
+
+    auto empty_query = g.get_haplogroups_with_sites(7, {0, 1, 2, 3});
+    EXPECT_EQ(empty_query, AlleleIds{});
+}
+
+using ::testing::InSequence;
+using ::testing::Return;
+TEST(LevelGenotyperInvalidation, GivenNestingStructure_CorrectGenotypeNullifying){
+    parental_map par_map{
+            {7, VariantLocus{5, 1}},
+            {9, VariantLocus{7, 2}},
+    };
+    child_map child_m = build_child_map(par_map);
+
+    gt_sites sites(3);
+    auto site1 = std::make_shared<MockGenotypedSite>();
+    EXPECT_CALL(*site1, make_null())
+            .Times(1);
+    EXPECT_CALL(*site1, is_null())
+            .WillOnce(Return(false));
+    site1->set_num_haplogroups(5);
+    sites.at(1) = site1;
+
+    // SiteID 9 will get nulled by site 7.
+    // Then when site 5 nulls site 7, I want site 9 to signal it is already nulled.
+    auto site2 = std::make_shared<MockGenotypedSite>();
+    {
+        InSequence seq;
+        EXPECT_CALL(*site2, is_null())
+                .WillOnce(Return(false));
+        EXPECT_CALL(*site2, make_null())
+                .Times(1);
+        EXPECT_CALL(*site2, is_null())
+                .WillOnce(Return(true));
+    }
+    site2->set_num_haplogroups(5);
+    sites.at(2) = site2;
+
+    LevelGenotyper g{child_m, sites};
+
+    // I want site 9 to be invalidated in this call
+    g.invalidate_if_needed(7, AlleleIds{1});
+    // And this call to invalidate site 7, without attempting to invalidate site 9 which was already so by above call.
+    g.invalidate_if_needed(5, AlleleIds{0});
 }
