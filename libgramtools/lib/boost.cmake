950993ca0b13d0ccfe06c51292c386980e23f2a7
bricoletc <bletcher@ebi.ac.uk>
2020-04-07 16:30:05 +0100

    Upgrade boost and remove system library dependency

--- libgramtools/lib/boost.cmake
+++ libgramtools/lib/boost.cmake
@@ -1,18 +1,16 @@
-set(BOOST_SRC_DIR ${CMAKE_CURRENT_BINARY_DIR}/src/boost_1_65_1)
-
-execute_process(COMMAND mkdir -p ${BOOST_SRC_DIR})
+set(BOOST_SRC_DIR ${CMAKE_CURRENT_BINARY_DIR}/src/boost_1_66_0)
 
 ExternalProject_Add(boost
-        DOWNLOAD_COMMAND  wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz --timestamping
+        DOWNLOAD_COMMAND  wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz --timestamping
         DOWNLOAD_DIR      "${CMAKE_CURRENT_BINARY_DIR}/download"
         CONFIGURE_COMMAND bash -c "cd ${BOOST_SRC_DIR} && ./bootstrap.sh \
-                        --with-libraries=program_options,timer,system,serialization \
-                        --prefix=${BOOST_SRC_DIR} 1>/dev/null"
+                        --with-libraries=chrono,program_options,serialization,timer \
+                        --prefix=${CMAKE_CURRENT_BINARY_DIR} 1>/dev/null"
         BUILD_COMMAND     bash -c "cd ${BOOST_SRC_DIR} && ./bjam install link=static 1>/dev/null"
         INSTALL_COMMAND   ""
         TEST_COMMAND      "")
 
 ExternalProject_Add_Step(boost extract_tar
-        COMMAND tar -xzf ${CMAKE_CURRENT_BINARY_DIR}/download/boost_1_65_1.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR}/src
+        COMMAND tar -xzf ${CMAKE_CURRENT_BINARY_DIR}/download/boost_1_66_0.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR}/src
         DEPENDEES download
         DEPENDERS configure)
