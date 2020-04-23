8915d1dec0dbed62dea50267d2216f6a0d40b30b
bricoletc <bletcher@ebi.ac.uk>
2020-04-23 12:18:11 +0100

    Add boost iostreams

--- libgramtools/lib/boost.cmake
+++ libgramtools/lib/boost.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(boost
         DOWNLOAD_COMMAND  wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz --timestamping
         DOWNLOAD_DIR      "${CMAKE_CURRENT_BINARY_DIR}/download"
         CONFIGURE_COMMAND bash -c "cd ${BOOST_SRC_DIR} && ./bootstrap.sh \
-                        --with-libraries=chrono,program_options,serialization,timer \
+                        --with-libraries=chrono,program_options,serialization,timer,iostreams \
                         --prefix=${CMAKE_CURRENT_BINARY_DIR} 1>/dev/null"
         BUILD_COMMAND     bash -c "cd ${BOOST_SRC_DIR} && ./bjam install link=static 1>/dev/null"
         INSTALL_COMMAND   ""
