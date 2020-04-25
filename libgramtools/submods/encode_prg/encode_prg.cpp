cf3022784826ff726e655699a38ec9b5d23c4a00
bricoletc <bletcher@ebi.ac.uk>
2020-04-25 20:20:57 +0100

    encode_prg submod takes input from stdin
    Means it can take much larger strings
    Also can take fasta as input

--- libgramtools/submods/encode_prg/encode_prg.cpp
+++ libgramtools/submods/encode_prg/encode_prg.cpp
@@ -9,25 +9,37 @@
  */
 
 #include <iostream>
+
 #include "prg/linearised_prg.hpp"
 
 void usage(const char* argv[]){
-    std::cout << "Usage: " << argv[0] << " prg_string -o PATH" << std::endl;
+    std::cout << "Usage: " << argv[0] << " -o PATH" << std::endl;
+    std::cout << "Provide input sequence on stdin, can be fasta" << std::endl;
     std::cout << "The prg string variant markers should be encoded using '[', ']' and ','" << std::endl;
+    std::cout << "View output using:  hexdump -v -e '1/4 \"%d \"' " << std::endl;
     exit(1);
 }
 
 int main(int argc, const char* argv[]) {
-    if (argc != 4) usage(argv);
-
-    std::string prg_string = argv[1];
-    std::transform(prg_string.begin(), prg_string.end(), prg_string.begin(), ::toupper);
-    auto as_marker_vec = prg_string_to_ints(prg_string);
+    if (argc != 3) usage(argv);
 
     std::ofstream fout;
-    std::string flag{argv[2]};
+    std::string flag{argv[1]};
     if (flag != "-o") usage(argv);
-    std::string fout_path = argv[3];
+    std::string fout_path = argv[2];
+
+    std::string line, prg_string;
+    while (std::cin){
+        std::getline(std::cin, line);
+        if (line[0] == '>') continue;
+        std::transform(line.begin(), line.end(), line.begin(), ::toupper);
+        prg_string += line;
+    }
+
+    if (prg_string.empty()) usage(argv);
+
+    auto as_marker_vec = prg_string_to_ints(prg_string);
+
     PRG_String handler(as_marker_vec);
     handler.write(fout_path);
 
