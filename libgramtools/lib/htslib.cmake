e26ac88349789bdbb05ffbf55f199675b417c24b
bricoletc <bletcher@ebi.ac.uk>
2020-05-29 22:29:40 +0100

    Compile htslib without CRAM support

--- libgramtools/lib/htslib.cmake
+++ libgramtools/lib/htslib.cmake
@@ -1,9 +1,10 @@
-execute_process(COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/src/htslib-1.6)
-
 ExternalProject_Add(htslib
         DOWNLOAD_COMMAND  wget https://github.com/samtools/htslib/releases/download/1.10/htslib-1.10.tar.bz2 --timestamping
         DOWNLOAD_DIR      "${CMAKE_CURRENT_BINARY_DIR}/download"
-        CONFIGURE_COMMAND bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/src/htslib-1.10 && autoheader && autoconf && env CPPFLAGS=-I${CMAKE_CURRENT_BINARY_DIR}/include LDFLAGS=-L${CMAKE_CURRENT_BINARY_DIR}/lib ./configure --prefix=${CMAKE_CURRENT_BINARY_DIR}"
+        CONFIGURE_COMMAND bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/src/htslib-1.10 && \
+            autoheader && autoconf && env CPPFLAGS=-I${CMAKE_CURRENT_BINARY_DIR}/include \
+            LDFLAGS=-L${CMAKE_CURRENT_BINARY_DIR}/lib \
+            ./configure --prefix=${CMAKE_CURRENT_BINARY_DIR} --disable-bz2 --disable-lzma"
         BUILD_COMMAND     bash -c "cd ${CMAKE_CURRENT_BINARY_DIR}/src/htslib-1.10 && make && make install"
         INSTALL_COMMAND   ""
         TEST_COMMAND      "")
