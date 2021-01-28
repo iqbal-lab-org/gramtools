8cecc5d06e0d040e7e5168803495b12c07ab5e41
bricoletc <bletcher@ebi.ac.uk>
2021-01-28 14:59:39 +0000

    New Dockerfile and updated README
    * Dockefile successfully builds docker image
    * README now points to docker container on quay.io as most convenient installation
    * README explains install from release vs source and mentions `cortex`
      dependencies, including python2.
    * Add TOC to README
    * Bugfix: conditional `test_main` target in backend

--- containers/docker/install.sh
+++ containers/docker/install.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
+set -e
+
+# Notes
+# * Dependencies:
+#       * cortex: used in variant discovery. depends on:
+#               * stampy: requires python2, python-dev
+#               * r-base
 
 apt-get update
 apt-get install -y \
     build-essential \
-    libbz2-dev \
-    liblzma-dev \
     cmake \
-    git \
-    python3-pip \
-    autoconf \
-    sudo \
-    wget
+	automake \
+	git \
+	liblzma-dev \
+	libbz2-dev \
+	zlib1g-dev \
+	pkg-config \
+	python3 \
+	python3-pip \
+	python \
+	python-dev \
+	r-base \
+	wget
 
-sudo pip3 install git+https://github.com/iqbal-lab-org/gramtools
+pip3 install pip==20.0.2 # upgrade pip
+pip3 install .
 
 apt-get autoremove --purge -y \
+    build-essential \
     cmake \
     git \
     autoconf \
