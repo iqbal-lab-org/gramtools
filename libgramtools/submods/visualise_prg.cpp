881c6cec46ef238f1e67e80746fe1284d3531592
bricoletc <bletcher@ebi.ac.uk>
2021-05-18 17:27:53 +0100

    Upgraded visualise_prg binary
    Now can take in a region CLI argument (chr:start-stop) and draws the subgraph in that
    region

--- libgramtools/submods/visualise_prg.cpp
+++ libgramtools/submods/visualise_prg.cpp
@@ -6,39 +6,145 @@
  */
 #include <iostream>
 #include <map>
+#include <optional>
+#include <regex>
 #include <string>
 #include <vector>
 
+#include "genotype/infer/output_specs/segment_tracker.hpp"
 #include "prg/coverage_graph.hpp"
-
 #include "submod_resources.hpp"
 
+using gram::genotype::SegmentTracker;
+using gram::submods::covG_ptrPair;
+
 void usage(const char* argv[]) {
-  std::cout << "Usage: " << argv[0] << " coverage_graph start stop outfile"
+  std::cout << "Usage: " << argv[0] << " coverage_graph region [coords_file]"
             << std::endl;
   std::cout << "\t coverage_graph: produced by gramtools build." << std::endl;
-  std::cout << "\t start: index of first site to visualise" << std::endl;
-  std::cout << "\t stop: index of last site to visualise. can be same as start."
+  std::cout << "\t region: description of subgraph to extract." << std::endl;
+  std::cout << "\t\t region must be of form: 'chrom:start-stop' (for a genomic "
+               "region) \n \t\t or "
+               "'start-stop' (for site indices in the prg; 0-based, inclusive)."
+            << std::endl;
+  std::cout << "\t coords_file: produced by gramtools build. required if "
+               "region is a genomic region."
             << std::endl;
   exit(1);
 }
 
+struct MatchRegion {
+  std::string chrom;
+  std::size_t start_location, end_location;
+};
+
+std::optional<MatchRegion> regexp_match_region(
+    std::string const& region_string) {
+  std::regex const genomic_region_regex("(.+):([0-9]+)-([0-9]+)");
+  std::regex const site_index_region_regex("([0-9]+)-([0-9]+)");
+  std::smatch pieces_match;
+  std::size_t start_location, end_location;
+
+  if (std::regex_match(region_string, pieces_match, genomic_region_regex)) {
+    std::string chrom = pieces_match[1];
+    start_location = stoi(pieces_match[2]);
+    end_location = stoi(pieces_match[3]);
+    return MatchRegion{chrom, start_location, end_location};
+  } else if (std::regex_match(region_string, pieces_match,
+                              site_index_region_regex)) {
+    start_location = stoi(pieces_match[1]);
+    end_location = stoi(pieces_match[2]);
+    return MatchRegion{"", start_location, end_location};
+  } else
+    return {};
+}
+
+bool is_in_node(std::size_t query_pos, covG_ptr const& node,
+                SegmentTracker& tracker) {
+  auto const node_start = tracker.get_relative_pos(node->get_pos());
+  auto const seq_size = node->get_sequence_size();
+  auto node_stop = node_start;
+  if (seq_size > 0) node_stop += seq_size - 1;
+  return (query_pos >= node_start && query_pos <= node_stop);
+}
+
+std::optional<covG_ptrPair> find_nodes_by_genomic_region(
+    MatchRegion const& match_region, covG_ptr const& root_node,
+    SegmentTracker& tracker) {
+  covG_ptr start_node, stop_node;
+  std::size_t focal_position = match_region.start_location;
+  covG_ptr cur_node = root_node;
+  std::string chrom;
+  while (cur_node->get_num_edges() > 0) {
+    cur_node = cur_node->get_edges()[0];
+    // TODO: below check is necessary because the sink node
+    // is currently set to have the largest prg position + 1, triggering tracker
+    // query to fail when no match found in the graph
+    if (cur_node->get_num_edges() == 0) break;
+    chrom = tracker.get_ID(cur_node->get_pos());
+    if (chrom != match_region.chrom) continue;
+    if (is_in_node(focal_position, cur_node, tracker)) {
+      if (focal_position == match_region.start_location) {
+        focal_position = match_region.end_location;
+        start_node = cur_node;
+      } else
+        return covG_ptrPair{start_node, cur_node};
+    }
+  }
+  return {};
+}
+
+covG_ptrPair find_nodes_by_site_idx(std::size_t const start_index,
+                                    std::size_t const end_index,
+                                    coverage_Graph const& graph) {
+  auto num_var_sites = graph.bubble_map.size();
+  if (start_index >= num_var_sites || end_index >= num_var_sites) {
+    std::cout << "Error: there are only " + std::to_string(num_var_sites) +
+                     " variant sites in the prg.\n";
+    exit(1);
+  }
+  covG_ptr start_node, stop_node;
+  auto start_pair = gram::submods::get_bubble_nodes(
+      graph.bubble_map, index_to_siteID(start_index));
+  start_node = start_pair.first;
+  if (start_index != end_index)
+    stop_node = gram::submods::get_bubble_nodes(graph.bubble_map,
+                                                index_to_siteID(end_index))
+                    .second;
+  else
+    stop_node = start_pair.second;
+  return {start_node, stop_node};
+}
+
 int main(int argc, const char* argv[]) {
-  if (argc != 5) usage(argv);
+  if (argc < 3 || argc > 4) usage(argv);
 
   // Argument parsing and validation
-  int start_idx = atoi(argv[2]);
-  int stop_idx = atoi(argv[3]);
-  if (start_idx < 0 || stop_idx < 0 || (stop_idx < start_idx)) {
-    std::cout << "Error: start and stop must be >0 and stop >= start \n";
+  auto matched_region = regexp_match_region(argv[2]);
+  if (!matched_region) {
+    std::cout << "Error: invalid search region " << argv[2] << std::endl;
     usage(argv);
   }
 
-  std::string ofprefix{argv[4]};
-  std::ofstream ofs{ofprefix + ".gv"};
-  if (!ofs.good()) {
-    std::cout << "Error: could not open " << argv[4] << std::endl;
+  auto const start_location = matched_region->start_location;
+  auto const end_location = matched_region->end_location;
+  if (start_location > end_location) {
+    std::cout << "Error: start must be <= stop \n";
     usage(argv);
+  }
+
+  SegmentTracker tracker;
+  if (matched_region->chrom.size() != 0) {
+    if (argc != 4) {
+      std::cout << "Error: missing coords_file" << std::endl;
+      usage(argv);
+    }
+    std::ifstream ifs{argv[3]};
+    if (!ifs.good()) {
+      std::cout << "Error: could not open " << argv[3] << std::endl;
+      usage(argv);
+    }
+    tracker = SegmentTracker(ifs);
   }
 
   std::ifstream ifs{argv[1]};
@@ -50,24 +156,22 @@ int main(int argc, const char* argv[]) {
   coverage_Graph graph;
   ia >> graph;
 
-  auto num_var_sites = graph.bubble_map.size();
-  if (start_idx >= num_var_sites || stop_idx >= num_var_sites) {
-    std::cout << "Error: there are only " + std::to_string(num_var_sites) +
-                     " variant sites in the prg.\n";
-    usage(argv);
+  covG_ptrPair node_pair;
+  if (matched_region->chrom.size() != 0) {
+    auto result = find_nodes_by_genomic_region(matched_region.value(),
+                                               graph.root, tracker);
+    if (!result) {
+      std::cout << "Error: could not find nodes spanning " << argv[2] << "\n";
+      exit(1);
+    }
+    node_pair = result.value();
+  } else {
+    node_pair = find_nodes_by_site_idx(start_location, end_location, graph);
   }
 
-  covG_ptr start_node, stop_node;
-  auto start_pair = gram::submods::get_bubble_nodes(graph.bubble_map,
-                                                    index_to_siteID(start_idx));
-  start_node = start_pair.first;
-  if (start_idx != stop_idx)
-    stop_node = gram::submods::get_bubble_nodes(graph.bubble_map,
-                                                index_to_siteID(stop_idx))
-                    .second;
-  else
-    stop_node = start_pair.second;
-
+  // Write subgraph by visiting all nodes inside the node pair
+  auto start_node = node_pair.first;
+  auto stop_node = node_pair.second;
   std::size_t new_idx{0};
   std::map<covG_ptr, std::size_t> node_ids{{start_node, new_idx++}};
   std::vector<covG_ptr> to_visit{start_node};
@@ -122,5 +226,5 @@ int main(int argc, const char* argv[]) {
     }
   }
 
-  ofs << "digraph \"" << ofprefix << "\" {\n" << nodes << edges << "\n}";
+  std::cout << "digraph \"gramtools_subgraph\" {\n" << nodes << edges << "\n}";
 }
